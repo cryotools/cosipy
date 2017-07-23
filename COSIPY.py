7c3a977de69d2e3b6fe6ddb1db16a9f1def55e8e
anz <anselm.arndt@geo.hu-berlin.de>
2017-07-23 13:06:23 +0200

    change letters because snow scripts are not any more capatalized

--- COSIPY.py
+++ COSIPY.py
@@ -26,23 +26,24 @@
 
 # from jackedCodeTimerPY import JackedTiming
 import numpy as np
-import matplotlib.pylab as plt
-from config import *
-from Constants import *
-from inputData import *
-import Grid as grd
-from heatEquationLagrange import solveHeatEquation
-from albedo import updateAlbedo
-from roughness import updateRoughness
-from surfaceTemperature import updateSurfaceTemperature
-from penetratingRadiation import penetratingRadiation
-from percolation import percolation
 
+from modules.heatEquation import solveHeatEquation
+from modules.penetratingRadiation import penetratingRadiation
+from modules.roughness import updateRoughness
+from modules.surfaceTemperature import updateSurfaceTemperature
+from modules.albedo import updateAlbedo
+from modules.percolation import percolation
+
+import dyn.grid as grd
+from dyn.io import *
+
+from constants import *
+from namelist import *
 
 ' model function '
 
-def main():
 
+def main():
     # JTime = JackedTiming()
 
     evdiff = 0  # Auxiliary Variables - hours since last snowfall
@@ -52,18 +53,18 @@ def main():
     nlay = 10
 
     # Init layers
-    hlayers = 0.1*np.ones(nlay)
+    hlayers = 0.1 * np.ones(nlay)
 
-    rho = pice*np.ones(nlay)
+    rho = pice * np.ones(nlay)
     rho[0] = 250.
     rho[1] = 400.
     rho[2] = 550.
-    
+
     # Init temp
-    Ts = Tb*np.ones(nlay)
+    Ts = Tb * np.ones(nlay)
     for i in range(len(Ts)):
         gradient = ((T2[0] - Tb) / (nlay))
-        Ts[i] = T2[0] - gradient*i
+        Ts[i] = T2[0] - gradient * i
 
     # Init LWC
     LWC = np.zeros(nlay)
@@ -83,7 +84,7 @@ def main():
     # Merge grid layers, if necessary
     GRID.update_grid(mergingLevel)
 
-    Hall = []               # todo variable documentation
+    Hall = []  # todo variable documentation
     Lall = []
     Liall = []
     Loall = []
@@ -104,16 +105,14 @@ def main():
         Hsnow = Hsnow + snowfall[t]
 
         if (snowfall[t] > 0.0):
-
             # TODO: Better use weq than snowheight
 
             # Add a new snow node on top
             GRID.add_node(float(DATA['snowfall'][t]), rho_new, float(DATA['T2'][t]), 0)
             GRID.mergeNewSnow(mergeNewSnowThreshold)
 
-
         if (snowfall[t] < 0.005):
-            evdiff = evdiff + (dt/3600.0)
+            evdiff = evdiff + (dt / 3600.0)
         else:
             evdiff = 0
 
@@ -128,29 +127,29 @@ def main():
         GRID.update_grid(mergingLevel)
 
         # Solve the heat equation
-        solveHeatEquation(GRID, dt)    
+        solveHeatEquation(GRID, dt)
 
         # Find new surface temperature
         fun, T0, Li, Lo, H, L, B, SWnet = updateSurfaceTemperature(GRID, alpha, z0, t)
 
         # Surface fluxes [m w.e.q.]
         if (GRID.get_T_node(0) < zeroT):
-            sublimation = max(L/(1000.0*L_ms), 0) * dt
-            deposition = min(L/(1000.0*L_ms), 0) * dt
+            sublimation = max(L / (1000.0 * L_ms), 0) * dt
+            deposition = min(L / (1000.0 * L_ms), 0) * dt
             evaporation = 0
             condensation = 0
         else:
-            evaporation = max(L/(1000.0*L_mv), 0) * dt
-            condensation = min(L/(1000.0*L_mv), 0) * dt
+            evaporation = max(L / (1000.0 * L_mv), 0) * dt
+            condensation = min(L / (1000.0 * L_mv), 0) * dt
             sublimation = 0
             deposition = 0
 
         # Melt energy in [m w.e.q.]
-        meltEnergy =  max(0, SWnet+Li+Lo-B-H-L)     # W m^-2 / J s^-1 ^m-2
-        melt = meltEnergy*dt/(1000*L_m)             # m w.e.q. (ice)
+        meltEnergy = max(0, SWnet + Li + Lo - B - H - L)  # W m^-2 / J s^-1 ^m-2
+        melt = meltEnergy * dt / (1000 * L_m)  # m w.e.q. (ice)
 
         # Remove melt height from surface and store as runoff (R)
-        GRID.removeMeltEnergy(melt+sublimation+deposition+evaporation+condensation)
+        GRID.removeMeltEnergy(melt + sublimation + deposition + evaporation + condensation)
 
         # Merge first layer, if too small (for model stability)
         GRID.mergeNewSnow(mergeNewSnowThreshold)
@@ -165,10 +164,10 @@ def main():
         # write single variables to output variables
         Liall.append(Li)  # in long rad
         Loall.append(Lo)  # out long rad
-        Hall.append(H)    # sensible heat flux *
-        Lall.append(L)    # latent heat flux   *
-        Ball.append(B)    # ground heat flux
-        T0all.append(T0)    # surface Temperature
+        Hall.append(H)  # sensible heat flux *
+        Lall.append(L)  # latent heat flux   *
+        Ball.append(B)  # ground heat flux
+        T0all.append(T0)  # surface Temperature
         SWall.append(SWnet)  # Surface net radiation
         Alphaall.append(alpha)  # albedo
         snowHeightall.append(np.sum((GRID.get_hlayer())))
@@ -178,6 +177,7 @@ def main():
     GRID.info()
 
     # print(JTime.report())
+
 
 ''' model execution '''
 
