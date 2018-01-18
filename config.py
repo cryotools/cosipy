cb64da34a3a82eafa66e193ea435cc944773ca1f
Anselm-Arndt <anselm.arndt@geo.hu-berlin.de>
2018-01-18 11:22:50 +0100

    Changed configuration paths and example in- and output

--- config.py
+++ config.py
@@ -2,24 +2,30 @@
  This is the COSIPY configuration (init) file.
  Please make your changes here.
 """
+from pathlib import Path
 
 ''' REQUIRED INPUT '''
-### 2D example
-input_example_2D=''
-output_example_2D= ''
+home = str(Path.home())
+folder='/Seafile/diss/io/'
 
-### 1D example
-input_example_1D = 'input/input_COSIPY-example.nc'
-output_example_1D = 'output/output_COSIPY-example.nc'
+# example Halji 2D Setup
+input_example_2D='input_COSIPY2D-mask_values-example.nc'
+output_example_2D= 'output_COSIPY2D-mask_values-example.nc'
 
-input_netcdf=input_example_1D
-output_netcdf=output_example_1D
+# example 2D Setup
+input_example_1D = 'input_COSIPY-example.nc'
+output_example_1D = 'output_COSIPY-example.nc'
 
-mat_path = 'input/input_COSIPY-example.mat'         # same input file as matlab file; obsolete?
+# example 1D Setup
+input_Halji = 'input_prepro_HAR_Halji_10_2000-10_2011_ohneKorrekturen.nc'
+output_Halji = 'output_HAR_Halji_10_2000-10_2011_ohneKorrekturen.nc'
 
+# change last example for different setups!
+input_netcdf=home+folder+input_example_1D
+output_netcdf=home+folder+output_example_1D
 
 time_start = 0                                      # time index to start
-time_end = 10                                       # len(T2) usually the length of the time series
+time_end = 7200                                     # len(T2) usually the length of the time series
 dt = 3600                                           # 3600, 7200, 10800 [s] length of time step per iteration in seconds
 
 debug_level = 0                                     # DEBUG levels: 0, 10, 20, 30
