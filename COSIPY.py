384c1df10b5a163bae2cc4ed9c0815bd0cd8fa0a
Tobias Sauter <tobias.sauter@uibk.ac.at>
2018-05-31 13:11:52 +0200

    New disclaimer

--- COSIPY.py
+++ COSIPY.py
@@ -2,18 +2,15 @@
 
 """
     This is the main code file of the 'COupled Snowpack and Ice surface energy
-    and MAss balance glacier model in Python' (COSIPY). The model is orientated and inspired
-    by the COupled Snowpack and Ice surface energy and MAss balance model (COSIMA)
-    which was written in Matlab by Huintjes et al. (2015).
+    and MAss balance glacier model in Python' (COSIPY). The model was initially written by
+    Tobias Sauter and further developed by others.
+    
+    Developers:
 
-    The Python translation and model improvement of COSIMA was done by
-    [in allhabetical order]
+    Tobias Sauter
     Anselm Arndt
     David Loibl
     Bjoern Sass
-    Tobias Sauter
-
-    The python version of the model is subsequently called COSIPY.
 
     You are allowed to use and modify this code in a noncommercial manner and by
     appropriately citing the above mentioned developers.
@@ -24,7 +21,8 @@
 
     The model is written in Python 3.6.3 and is tested on Anaconda3-4.4.7 64-bit.
 
-    Correspondence: anselm.arndt@geo.hu-berlin.de
+    Correspondence: tobias.sauter@fau.de
+
 """
 from datetime import datetime
 from itertools import product
