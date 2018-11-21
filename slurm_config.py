99c2cb2d3bb2e8c6ddfb3c28d760bde37a92aa1c
anz <anselm.arndt@geo.hu-berlin.de>
2018-11-21 12:53:33 +0100

    correction cs2cosipy.py

--- slurm_config.py
+++ slurm_config.py
@@ -8,7 +8,7 @@ cores = 1                 ### 1 gridpoint on one core is one job
 processes = 1             ### 1 process pro core
 memory = '2GB'            ### memoroy per job
 slurm_parameters = ['--qos=short', '--job-name="Argog"','--account=prime',
-                    '--output=/data/scratch/arndtans/logs/1_COSIPY_Argog.out',
-                    '--error=/data/scratch/arndtans/logs/1_COSIPY_Argog.err'
+                    '--output=',
+                    '--error='
                     ]
 slurm_scale = 100
