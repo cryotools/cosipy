5e300c454e3cf6e5c99cac8fa0770b7771f403d9
anz <anselm.arndt@geo.hu-berlin.de>
2018-11-29 17:26:25 +0100

    Changes that Warning or Error if time start and time end are not in periode of input data, display glacier gridpoints; chnages to constants, avoid hard coded numbers in functions, change slurm config, add script for creation of static file, change example static files, add csv2cosipy.py and config for more general use, addition to cs2cosipy.py

---
+++ utilities/csv2cosipyConfig.py
@@ -0,0 +1,53 @@
+"""
+ This is the configuration (init) file for the utility cs2posipy.
+ Please make your changes here.
+"""
+
+#------------------------
+# Declare variable names 
+#------------------------
+
+# Temperature
+T_var = 'T2'  
+
+# Relative humidity
+RH_var = 'RH2'   
+
+# Wind velocity
+U_var = 'U2'     
+
+# Precipitation
+RRR_var = 'RRR' 
+
+# Incoming shortwave radiation
+G_var = 'G'   
+
+# Incoming longwave radiation
+LW_var = ''   
+
+# Pressure
+P_var = 'PRES'   
+
+# Cloud cover fraction
+N_var = 'N'   
+
+#------------------------
+# Radiation module 
+#------------------------
+radiationModule = True 
+
+# Time zone
+timezone_lon = 90.0
+
+# Zenit threshold (>threshold == zenit)
+zeni_thld = 86.0
+
+#------------------------
+# Interpolation arguments 
+#------------------------
+stationName = 'Halji'
+stationAlt = 5430
+
+lapse_T    = -0.006  # Temp K per  m
+lapse_RH   = 0.001  # RH % per  m (0 to 1)
+lapse_RRR  = 0.001   # RRR % per m (0 to 1)
