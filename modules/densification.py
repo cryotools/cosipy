eb53a1ef9fe1b5b029fadc735a39bb32c9e78c6f
Anselm Arndt <33282497+AnsArn@users.noreply.github.com>
2018-12-04 19:29:11 +0100

    Update densification.py

--- modules/densification.py
+++ modules/densification.py
@@ -1,14 +1,18 @@
 import numpy as np
 from constants import *
-from config import *
 
-<<<<<<< HEAD
-
-def densification(GRID, t, debug_level):
+def densification(GRID):
     """ Densification of the snowpack
-
     Args:
-=======
+        GRID    ::  GRID-Structure
+    """
+
+# % Description: Densification through overburden pressure
+# % after Reijmer & Hock (2007) and Herron & Langway (1980)
+# % RETURNS:
+# % rho_snow   :: densitiy profile after densification    [m3/kg]
+# % h_diff     :: difference in height before and after densification [m]
+
 # Loop over all internal grid points
     ### get copy of layer heights and layer densities
     height_layers = GRID.get_height()
@@ -20,10 +24,16 @@ def densification(GRID, t, debug_level):
             weight = (GRID.get_node_height(idxNode)*0.5*GRID.get_node_density(idxNode))
         else:
             weight = (np.nansum(height_layers[0:idxNode-1]*density_temp[0:idxNode-1]))
->>>>>>> cosipyV1.0
 
-        GRID    ::  GRID-Structure 
-        t       ::  Integration time
+        if GRID.get_node_density(idxNode) < snow_firn_threshold:
+            density_temp[idxNode] = GRID.get_node_density(idxNode) + K0 * np.exp(-E0/(R * GRID.get_node_temperature(idxNode)))\
+            * weight * ((ice_density-GRID.get_node_density(idxNode))/ice_density)
 
-    """
+        elif (GRID.get_node_density(idxNode) > snow_firn_threshold) and (GRID.get_node_density(idxNode) < ice_density):
+            density_temp[idxNode] = GRID.get_node_density(idxNode) + K1 * np.exp(-E1 / (R * GRID.get_node_temperature(idxNode))) \
+                                    * weight * ((ice_density - GRID.get_node_density(idxNode)) / ice_density)
+        else:
+            density_temp[idxNode] = ice_density
 
+    GRID.set_height((GRID.get_density()/density_temp)*GRID.get_height())
+    GRID.set_density(density_temp)
