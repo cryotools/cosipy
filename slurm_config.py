e5c0ddc5fa793900f3dd8a7663b1c4ab4828e33d
AnsArn <anselm.arndt@geo.hu-berlin.de>
2019-03-17 08:38:28 +0100

    change rights, delete file....

--- slurm_config.py
+++ slurm_config.py
@@ -3,15 +3,15 @@
  Please make your changes here.
 """
 
-processes = 20	          				                    # grid points submitted in one sbatch script
-nodes = 1					                            # how many sbatach scripts are submitted
-memory_per_process = 3					                    # memory per processes in GB
-project = ''                            					# equivalent to slurm parameter --account
-name = 'Test',                                              # equivalent to slurm parameter --job-name
+processes = 20	          				            # grid points submitted in one sbatch script
+nodes = 1					                    # how many sbatach scripts are submitted
+memory_per_process = 3					            # memory per processes in GB
+project = ''                            			    # equivalent to slurm parameter --account
+name = 'Test',                                                      # equivalent to slurm parameter --job-name
 extra_slurm_parameters = [ '--qos=short', 		            # Slurm quality of service
-                    	   '--output=Output_test.out',	    # Path slurm output file
-                    	   '--error=Error_test.err',	    # Path slurm error file
-		                    '--time=1-00:00:00',		    # Time limit for job
+                    	   '--output=Output_test.out',	            # Path slurm output file
+                    	   '--error=Error_test.err',	            # Path slurm error file
+                           '--time=1-00:00:00', 		    # Time limit for job
                    	 ]
-port_monitoring = 8786					                    # port for monitoring
-cores = 1                 				                    # One grid point per core, do not change
+port_monitoring = 8786				                    # port for monitoring
+cores = 1                 			                    # One grid point per core, do not change
