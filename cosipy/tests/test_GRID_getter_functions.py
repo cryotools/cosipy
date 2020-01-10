979b854035acefcb9517290070f4aa00b6e59aff
AnsArn <anselm.arndt@geo.hu-berlin.de>
2020-01-10 22:28:30 +0100

    Enlarge test for GRID getter functions

--- cosipy/tests/test_GRID_getter_functions.py
+++ cosipy/tests/test_GRID_getter_functions.py
@@ -1,5 +1,6 @@
 import os; import yaml; import logging
 from cosipy.cpkernel.grid import *
+from pytest import approx
 from COSIPY import start_logging
 
 layer_heights = [0.1, 0.2, 0.3, 0.5, 0.5]
@@ -12,8 +13,18 @@ layer_liquid_water = [0.0, 0.0, 0.0, 0.0, 0.0]
 #logging.basicConfig(level=logging.INFO)
 
 def test_grid_getter_functions():
-    GRID = Grid(layer_heights, layer_densities, layer_temperatures, layer_liquid_water)
-    GRID.remove_melt_weq(0.01)
+     GRID = Grid(layer_heights, layer_densities, layer_temperatures, layer_liquid_water)
+     assert GRID.get_height() == layer_heights
+     assert GRID.get_density() == approx(layer_densities, abs=1e-3)
+     assert GRID.get_temperature() == layer_temperatures
+     assert GRID.get_liquid_water_content() == layer_liquid_water
+     assert GRID.get_snow_heights() == layer_heights[0:3]
+     #assert GRID.get_ice_heights() == layer_heights[3:5]
+     assert GRID.get_node_height(0) == layer_heights[0]
+     assert GRID.get_node_density(0) == approx(layer_densities[0], abs=1e-3)
+     assert GRID.get_node_temperature(0) == layer_temperatures[0]
+
+    # GRID.remove_melt_weq(0.01)
 
     #########REMOVE_NODE
     #number_nodes_before = GRID.get_number_layers()
