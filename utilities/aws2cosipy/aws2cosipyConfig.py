cf5ee07baa02a3c7ad9dbbb2aec8d6295ae8b3a6
anz <anselm.arndt@geo.hu-berlin.de>
2020-04-23 18:59:30 +0200

    SWin values outside glacier shoudd be nan and not 0, correct behaviouer with LWin and N as input

--- utilities/aws2cosipy/aws2cosipyConfig.py
+++ utilities/aws2cosipy/aws2cosipyConfig.py
@@ -71,7 +71,7 @@ hgt = 3300
 stationName = 'Zhadang'
 stationAlt = 5665.0
 
-lapse_T         = -0.006  # Temp K per  m
-lapse_RH        = 0.002  # RH % per  m (0 to 1)
-lapse_RRR       = 0.0000   # RRR % per m (0 to 1)
-lapse_SNOWFALL  = 0.0000   # Snowfall % per m (0 to 1)
+lapse_T         = -0.006    # Temp K per  m
+lapse_RH        =  0.002    # RH % per  m (0 to 1)
+lapse_RRR       =  0.0000   # RRR % per m (0 to 1)
+lapse_SNOWFALL  =  0.0000   # Snowfall % per m (0 to 1)
