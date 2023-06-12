eba2ea20f27133d509f2cb4a5dc04b9eebb19424
gampnico <45390064+gampnico@users.noreply.github.com>
2023-06-12 08:42:03 +0200

    fix(tests): fix Node tests
    Run tests:
    
    ```
    coverage run -m --source=. pytest \
        ./cosipy/tests/test_NODE_getter_functions.py \
        ./cosipy/tests/test_NODE_setter_functions.py
    ```
    
    Fixes:
        - TypeError when passing list to Grid() instead of numba array.
    
    Adds:
        - Method in Node test classes to create a Node.
        - Coverage for existing methods.
        - Pytest markers.
    
    Refactors:
        - Redundant imports by importing ``constants.py`` instead.
        - Test functions into classes. No more overwritten attributes.
        - Module scope of Node objects and attributes by instantiating at function
          scope.
        - Duplicated code by using fixtures.
        - Equality assertions to use np.allclose instead of pytest.approx, per
          pytest documentation.
        - Duplicated tests into parametrised test functions.
        - Linting, docstrings.

--- cosipy/tests/test_NODE_setter_functions.py
+++ cosipy/tests/test_NODE_setter_functions.py
@@ -1,30 +1,76 @@
-from cosipy.cpkernel.node import *
-height = 0.1
-density = 200.
-temperature = 270
-lwc = 0.3
-ice_fraction = 0.4
-test_node = Node(height, density, temperature, lwc, ice_fraction)
+import numpy as np
 
-new_height = 1.
-new_temperature = 260.
-new_liquidwater_content = 0.5
-new_ice_fraction = 0.9
-new_refreeze = 0.01
+# import constants
+from cosipy.cpkernel.node import Node
 
-def test_node_setter_functions():
 
-    test_node.set_layer_height(new_height)
-    assert test_node.get_layer_height() == new_height
+class TestNodeSetter:
+    """Tests set methods for Node objects.
 
-    test_node.set_layer_temperature(new_temperature)
-    assert test_node.get_layer_temperature() == new_temperature
+    Attributes:
+        height (float): Layer height [:math:`m`].
+        density (float): Snow density [:math:`kg~m^{-3}`].
+        temperature (int): Layer temperature [:math:`K`].
+        lwc (float): Liquid water content [:math:`m~w.e.`].
+        ice_fraction (float): Volumetric ice fraction [-].
+        refreeze (float): Amount of refreezed water [:math:`m~w.e.`].
+        new_height (float): New layer height [:math:`m`].
+        new_density (float): New snow density [:math:`kg~m^{-3}`].
+        new_temperature (int): New layer temperature [:math:`K`].
+        new_lwc (float): New liquid water content [:math:`m~w.e.`].
+        new_ice_fraction (float): New volumetric ice fraction [-].
 
-    test_node.set_layer_liquid_water_content(new_liquidwater_content)
-    assert test_node.get_layer_liquid_water_content() == new_liquidwater_content
+    """
 
-    test_node.set_layer_ice_fraction(new_ice_fraction)
-    assert test_node.get_layer_ice_fraction() == new_ice_fraction
+    height = 0.1
+    density = 200.0
+    temperature = 270
+    lwc = 0.3
+    ice_fraction = 0.4
+    refreeze = 0.01
+    new_height = 1.0
+    new_temperature = 260.0
+    new_lwc = 0.5
+    new_ice_fraction = 0.9
 
-    test_node.set_layer_refreeze(new_refreeze)
-    assert test_node.get_layer_refreeze() == new_refreeze
+    def create_node(
+        self,
+        height: float = height,
+        density: float = density,
+        temperature: float = temperature,
+        lwc: float = lwc,
+        ice_fraction: float = ice_fraction,
+    ) -> Node:
+        """Instantiate a Node."""
+
+        node = Node(
+            height=height,
+            snow_density=density,
+            temperature=temperature,
+            liquid_water_content=lwc,
+        )
+        assert isinstance(node, Node)
+        node.set_layer_ice_fraction(ice_fraction)
+
+        return node
+
+    def test_create_node(self):
+        node = self.create_node()
+        assert isinstance(node, Node)
+
+    def test_node_setter_functions(self):
+        node = self.create_node()
+        node.set_layer_height(self.new_height)
+        assert np.isclose(node.get_layer_height(), self.new_height)
+
+        node.set_layer_temperature(self.new_temperature)
+        assert np.isclose(node.get_layer_temperature(), self.new_temperature)
+
+        node.set_layer_liquid_water_content(self.new_lwc)
+        assert np.isclose(node.get_layer_liquid_water_content(), self.new_lwc)
+
+        node.set_layer_ice_fraction(self.new_ice_fraction)
+        assert np.isclose(node.get_layer_ice_fraction(), self.new_ice_fraction)
+
+        node.set_layer_refreeze(self.refreeze)
+        assert np.isclose(node.get_layer_refreeze(), self.refreeze)
