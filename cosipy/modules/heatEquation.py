38781883405d0dc2784a5afa660aa47f9f0c70be
Marcus Gastaldello <116652649+MarcusGastaldello@users.noreply.github.com>
2024-02-02 17:17:45 +0100

    Add files via upload

--- cosipy/modules/heatEquation.py
+++ cosipy/modules/heatEquation.py
@@ -1,21 +1,30 @@
+from ast import Or
 import numpy as np
-from constants import basal_heat_flux
+from constants import basal_heat_flux, heat_equation_lower_boundary
 from numba import njit
 
-@njit
 def solveHeatEquation(GRID, dt):
+
+    heat_equation_lower_boundaries = ['basal_heat_flux','prescribed_temp']
+    if (heat_equation_lower_boundary == 'basal_heat_flux') or (heat_equation_lower_boundary == 'prescribed_temp'):
+        HeatEquation(GRID, dt)
+    else:
+        raise ValueError("heat_equation_lower_boundary = \"{:s}\" is not allowed, must be one of {:s}".format(heat_equation_lower_boundary, ", ".join(heat_equation_lower_boundaries)))
+
+@njit
+def HeatEquation(GRID, dt):
     """ Solves the heat equation on a non-uniform grid
 
     Constants:     
-                    dt    ::  Integration time in a model time-step (hour) [s]
-                    bhf   ::  Basal heat flux [W m-2]
+                dt    ::  Integration time in a model time-step (hour) [s]
+                bhf   ::  Basal heat flux [W m-2]
     Inputs:
-                    h(z)  ::  Layer height [m]
-                    K(z)  ::  Layer thermal diffusivity [m2 s-1]
-                    k(z)  ::  Layer thermal conductivity [W m-1 K-1]
-                    T(z)  ::  Layer temperature [K]    
+                h(z)  ::  Layer height [m]
+                K(z)  ::  Layer thermal diffusivity [m2 s-1]
+                k(z)  ::  Layer thermal conductivity [W m-1 K-1]
+                T(z)  ::  Layer temperature [K]    
     Outputs:
-                    T(z)  ::  Layer temperature (updated) [K]       
+                T(z)  ::  Layer temperature (updated) [K]       
     """
     
     # Get sub-surface layer properties:
@@ -28,7 +37,7 @@ def solveHeatEquation(GRID, dt):
     # Determine integration steps required in the solver to ensure numerical stability:
     stab_t = 0.0
     c_stab = 0.8
-    dt_stab  = c_stab * min(((z[1:]+z[:-1])/2)**2/(K[1:]+K[:-1]))
+    dt_stab  = c_stab * min(((h[1:]+h[:-1])/2)**2/(K[1:]+K[:-1]))
     
     # Numerically solve the Fourier heat equation using a finite central difference scheme in matrix form:
     while stab_t < dt:
@@ -36,14 +45,20 @@ def solveHeatEquation(GRID, dt):
         stab_t = stab_t + dt_use
 
         # Update the temperatures of the intermediate sub-surface nodes:
-        Tnew[1:-1] = T[1:-1] + dt_use * (((0.5 * (K[2:]  + K[1:-1]))  * (T[2:]   - T[1:-1]) / (0.5 * (z[2:]  + z[1:-1])) - \
-                                          (0.5 * (K[:-2] + K[1:-1]))  * (T[1:-1] - T[:-2])  / (0.5 * (z[:-2] + z[1:-1]))) / \
-                                          (0.25 * z[:-2]  + 0.5 * z[1:-1] + 0.25 * z[2:]))
-        # Update the temperature of the base sub-surface node:
-        Tnew[-1]   = T[-1]   + dt_use * (((basal_heat_flux * K[-1] / k[-1]) - \
-                                         ((0.5 * (K[-2] + K[-1])) * (T[-1] - T[-2])  / (0.5 * (z[-2] + z[-1])))) / \
-                                          (0.25 * z[-2] + 0.75 * z[-1]))
+        Tnew[1:-1] = T[1:-1] + dt_use * (((0.5 * (K[2:]  + K[1:-1]))  * (T[2:]   - T[1:-1]) / (0.5 * (h[2:]  + h[1:-1])) - \
+                                        (0.5 * (K[:-2] + K[1:-1]))  * (T[1:-1] - T[:-2])  / (0.5 * (h[:-2] + h[1:-1]))) / \
+                                        (0.25 * h[:-2]  + 0.5 * h[1:-1] + 0.25 * h[2:]))
+        
+        if heat_equation_lower_boundary == 'basal_heat_flux':
+
+            # Update the temperature of the base sub-surface node using the basal heat flux:
+            Tnew[-1]   = T[-1]   + dt_use * (((basal_heat_flux * K[-1] / k[-1]) - \
+                                            ((0.5 * (K[-2] + K[-1])) * (T[-1] - T[-2])  / (0.5 * (h[-2] + h[-1])))) / \
+                                            (0.25 * h[-2] + 0.75 * h[-1]))
+        
         T = Tnew.copy()
         
     # Write results to GRID
     GRID.set_temperature(T)
+
+
