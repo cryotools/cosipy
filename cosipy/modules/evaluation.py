ea766112065d6508212842c63e95ae0b8cb8a22d
gampnico <45390064+gampnico@users.noreply.github.com>
2024-10-15 11:48:20 +0200

    refactor(rmse)!: optimisations in evaluation module
    BREAKING CHANGE: `df_` is now `df` to avoid collisions in the
    documentation.

--- cosipy/modules/evaluation.py
+++ cosipy/modules/evaluation.py
@@ -1,32 +1,44 @@
 from cosipy.config import Config
 
 
-def evaluate(stake_names, stake_data, df_):
+def evaluate(stake_names, stake_data, df):
     """Evaluate the simulation using stake measurements.
 
     Args:
         stake_names (list): Stake IDs.
         stake_data (pd.Dataframe): Stake measurements.
         df (pd.Dataframe): Simulated mass balance and snow height.
-    
+
     Returns:
         Statistical evaluation.
     """
 
-    if Config.eval_method == 'rmse':
-        stat = rmse(stake_names, stake_data, df_)
+    if Config.eval_method == "rmse":
+        stat = rmse(stake_names, stake_data, df)
     else:
         stat = None
-       
+
     return stat
 
 
-def rmse(stake_names, stake_data, df_):
-    if (Config.obs_type=='mb'):
-        rmse = ((stake_data[stake_names].subtract(df_['mb'],axis=0))**2).mean()**.5
-    elif (Config.obs_type=='snowheight'):
-        rmse = ((stake_data[stake_names].subtract(df_['snowheight'],axis=0))**2).mean()**.5
-    else:
-        msg = f'RMSE not implemented for obs_type="{Config.obs_type}" in config.py.'
+def rmse(stake_names: list, stake_data, df) -> float:
+    """Get RMSE of simulated stake measurements.
+
+    Args:
+        stake_names: Stake IDs.
+        stake_data (pd.Dataframe): Stake measurements.
+        df (pd.Dataframe): Simulated mass balance and snow height.
+
+    Returns:
+        RMSE of simulated measurements.
+    """
+    if Config.obs_type not in ["mb", "snowheight"]:
+        msg = f'RMSE not implemented for obs_type="{Config.obs_type}" in config.toml.'
         raise NotImplementedError(msg)
+    else:
+        rmse = (
+            (stake_data[stake_names].subtract(df[Config.obs_type], axis=0))
+            ** 2
+        ).mean() ** 0.5
+
     return rmse
